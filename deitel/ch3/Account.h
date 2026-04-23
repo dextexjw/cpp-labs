8494044ed312a08a7415a630fc382b4bbb2cc570
Dexter <dev@jax22.com>
2026-04-22 20:15:59 -0400

    Enhance Account class withdrawal method with success and error messages; update exercise example to reflect changes.

--- deitel/ch3/Account.h
+++ deitel/ch3/Account.h
@@ -22,12 +22,14 @@ public:
     name = account_name;
   }
 
-  int withdraw(int withdraw_ammount){
-    if ( balance >= withdraw_ammount)
-      balance -= withdraw_ammount;
-    else
-     return 0;
-    return balance;
+  void withdraw(int withdrawal_amount) {
+    if (balance >= withdrawal_amount) {
+      balance -= withdrawal_amount;
+      std::cout << "Withdrawal of $" << withdrawal_amount << " successful. "
+                << "New balance: $" << balance << std::endl;
+    } else {
+      std::cout << "Withdrawal amount exceeded account balance." << std::endl;
+    }
   }
 
 private:
--- deitel/ch3/ch3-exercise.cpp
+++ deitel/ch3/ch3-exercise.cpp
@@ -4,13 +4,22 @@
 using namespace std;
 
 void ex3_9(){
-  Account acc1( 50, "Jamal Khan");
-  cout << acc1.getName();
+  Account acc1(50, "Jamal Khan");
+  
+  cout << "Account: " << acc1.getName() << ", Balance: $" << acc1.getBalance() << endl;
+  
+  // Test successful withdrawal
+  cout << "\nAttempting to withdraw $20..." << endl;
+  acc1.withdraw(20);
+  
+  // Test withdrawal that exceeds balance
+  cout << "\nAttempting to withdraw $50..." << endl;
+  acc1.withdraw(50);
+  
+  cout << "\nFinal balance: $" << acc1.getBalance() << endl;
 }
-int main (){
 
+int main(){
   ex3_9();
-
   return 0;
-
 }
